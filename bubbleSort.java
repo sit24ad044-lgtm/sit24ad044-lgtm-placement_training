7a86c8e2b26fd80f620f2f1e40001a020d9b94e3
sit24ad044-lgtm <sit24ad044@sairamtap.edu.in>
2026-08-03 09:22:41 +0530

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class bubbleSort{
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        in.close();
+    }
+}
